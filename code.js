74ef58faeee48e09eac36eff832a5423b4586713
adamchappell00 <adam.chappell00@gmail.com>
2021-10-15 12:31:41 -0500

    sayHello completed with null/object/empty strings tests passed

--- code.js
+++ code.js
@@ -3,5 +3,9 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(name) {
-    return "Hello " + name;
+    if (typeof name === "string" && name !== ""){
+        return "Hello " + name + "!";
+    } else if (typeof name === 'undefined' || typeof name === 'boolean' || typeof name === "object" || name === ""){
+        return "Hello World!";
+    }
 }
--- tests.js
+++ tests.js
@@ -1,3 +1,9 @@
+// Global definitions/instances
+var myObject = {
+    size : "Large",
+    state: 2
+}
+
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
@@ -27,7 +33,29 @@ describe('sayHello', function() {
     it('should return the string that contains the word "Hello" when executed', function() {
         expect(sayHello()).toContain("Hello");
     });
-    it('should return the string of "Hello Adam" when passed the Parameter of "Adam": sayHello("Adam")', function() {
-        expect(sayHello('Adam')).toBe("Hello Adam");
+    it('should return a string greeting the input', function() {
+        expect(sayHello("Adam")).toBe("Hello Adam!");
+        expect(sayHello("Jay")).toBe("Hello Jay!");
+    });
+    it('should return the string of "Hello World" when executed without an input', function() {
+        expect(sayHello()).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with a value of false as an input ', function() {
+        expect(sayHello(false)).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with a value of true as an input ', function() {
+        expect(sayHello(true)).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with a null value ', function() {
+        expect(sayHello(null)).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with an empty string: "" ', function() {
+        expect(sayHello("")).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with an Array', function() {
+        expect(sayHello([1])).toBe("Hello World!");
+    });
+    it('should return the string of "Hello World!" when executed with an Object that is not undefined', function() {
+        expect(sayHello(myObject)).toBe("Hello World!");
     });
 })
