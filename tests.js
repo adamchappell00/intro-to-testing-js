f5c7210bac749711161439f1b237b5ecc6bd4f43
adamchappell00 <adam.chappell00@gmail.com>
2021-10-15 11:24:23 -0500

    function sayHello written with initial tests

--- tests.js
+++ tests.js
@@ -13,3 +13,18 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello()).toBe("string");
+    })
+    it('should return a string when called', function() {
+        expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string that contains the word "Hello" when executed', function() {
+        expect(sayHello()).toContain("Hello");
+    });
+    it('should return the string of "Hello Adam" when passed the Parameter of "Adam": sayHello("Adam")', function() {
+        expect(sayHello('Adam')).toBe("Hello Adam");
+    });
+})
